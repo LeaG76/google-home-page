1c138e6d7d77beeb880b1eae9c7ef0e60c0e53cf
LeaG76 <leagallier.lag@gmail.com>
2024-03-14 11:49:35 +0100

    Update index.js

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,16 +1,11 @@
 var url = "google.com";
-var base = "https://leag76.github.io/google-home-page/"
+var base = "https://leag76.github.io/google-home-page/";
 
 function searchOnGoogle() {
     url = 'https://'+url+'/search?q='+inputValue('#inputSearch').value;
     window.open(url);
-    goBackHome();
 }
 
 function inputValue(e) {
     return document.querySelector(e);
-}
-
-function goBackHome() {
-    window.location.href = base;
 }
